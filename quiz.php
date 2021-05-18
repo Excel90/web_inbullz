f3bd58fc0676502ca6e9c16d95113c93dfc39e69
AllenXeon07 <franciscoallenxeon@gmail.com>
2021-05-18 22:12:49 +0700

    add timer

--- quiz.php
+++ quiz.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
+    <p>Timer : <span id="timer"></span></p>
     <form>
         <!-- no 1 -->
         <label for="no1">1. Otak dari sebuah komputer…?</label><br>
@@ -24,7 +25,38 @@
         <input type="radio" name="no2" value="d">d. Clrscrn<br>
         <br>
 
-        
+
     </form>
+
+
+    <script>
+// Set the date we're counting down to
+var countDownDate = new Date().getTime() + 7*60000 + 2*1000;
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = 
+   minutes + ":" + seconds ;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
 </body>
 </html>
